d2d7fd644ef202810448fdb266116315106a2fab
SamBrownCoding <166549412+SamBrownCoding@users.noreply.github.com>
2024-11-20 15:49:30 -0600

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Login from "./components/Login";
+import '../src/style.css'
+
+function App(){
+
+    return(
+        <div className="container">
+            <Login />
+        </div>
+    )
+}
+
+
+export default App;
